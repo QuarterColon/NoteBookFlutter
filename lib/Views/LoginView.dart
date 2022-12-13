58303fda5da3fc0547861fa6e218b8857ca279a2
Ankit Chakraborty <ankitchakraborty1972@gmail.com>
2022-12-13 22:43:24 +0530

    none

--- lib/Views/LoginView.dart
+++ lib/Views/LoginView.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:learning_dart/constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 
 
@@ -69,10 +68,20 @@ class _LoginViewState extends State<LoginView> {
                       (route) => false,);
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
-                  devtools.log("User Not Found");
+                  await showErrorDialog(context, 'User Not Found');
                 } else if (e.code == 'wrong-password') {
-                  devtools.log("Wrong password");
+                  await showErrorDialog(context, 'Wrong Password');
+                } else {
+                  await showErrorDialog(
+                      context,
+                      'Error: ${e.code}'
+                  );
                 }
+              } catch(e) {
+                await showErrorDialog(
+                    context,
+                    e.toString(),
+                );
               }
             },
             child: const Text("Login"),
