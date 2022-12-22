298c99b21fc8e9a195ac58430f99857ab61b1831
Ankit Chakraborty <ankitchakraborty1972@gmail.com>
2022-12-22 08:10:39 +0530

    HI

---
+++ lib/Services/auth/auth_provider.dart
@@ -0,0 +1,15 @@
+import 'package:learning_dart/Services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+  required String email,
+  required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
